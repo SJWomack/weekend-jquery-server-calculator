7d7f0201382e0891c32a5cff7938a5ab3f9ae7e9
Stephen Womack <swomack111@gmail.com>
2022-05-22 15:47:47 -0500

    wasn't feeling well and forgot to commit enough but finished base mode

--- server/modules/math.js
+++ server/modules/math.js
@@ -1,3 +1,20 @@
-module.exports ={
-    
-}
+module.exports = 
+    function mathOperations(operator,num1, num2){
+        if (operator === '*'){
+         let answer =   Number(num1) * Number(num2) 
+            return answer;
+        }
+       else if (operator === '/'){
+            let answer = Number(num1) / Number(num2) 
+               return answer;
+           }
+        else if (operator === '-'){
+            let answer =   Number(num1) - Number(num2) 
+               return answer;
+           }
+        else if (operator === '+'){
+            let answer =   Number(num1) + Number(num2) 
+               return answer;
+           }
+    }
+
--- server/public/client.js
+++ server/public/client.js
@@ -1,8 +1,91 @@
 $(onReady)
 
-function onReady(){
+function onReady() {
     console.log('R U readaaaaaaye')
-
+    handleGet();
     $('.calc-btn').on('click', onNumberOrOperatorClick);
+    $('#clear-display').on('click', handleClearDisplay);
+    $('#btn-equals').on('click', postPackageHandler)
+}
+
+let firstNum = '';
+let firstValBool = true;
+let secondNum = '';
+let operator = '';
+let postPackage = {};
+
+
+function appendCalcDisplay() {
+    console.log('in append calc')
+    $('#calc-display').val(firstNum + ' ' + operator + ' ' + secondNum)
+}
+
+function onNumberOrOperatorClick(evt) {
+    evt.preventDefault()
+    if ($(this).text() === '+' || $(this).text() === '-' ||$(this).text() ===  '*' || $(this).text() === '/') {
+        firstValBool = false;
+        console.log(firstValBool);
+
+        operator = $(this).text();
+        console.log(operator);
+    }
+    else if (firstValBool === true) {
+        firstNum += $(this).text();
+        console.log(firstNum);
+    }
+    else {
+        secondNum += $(this).text();
+        console.log(secondNum);
+    }
+
+    console.log( 'text value',$(this).text())
+
+    appendCalcDisplay();
+}
+
+function handleClearDisplay() {
+    $('#calc-display').val('');
+    firstNum = '';
+    firstValBool = true;
+    secondNum = '';
+    operator = '';
+
+}
+
+function postPackageHandler(evt) {
+    evt.preventDefault();
+    postPackage.firstNumber = firstNum;
+    postPackage.secondNumber = secondNum;
+    postPackage.operator = operator;
+    handleClearDisplay();
+    console.log(postPackage);
+    handlePost();
+}
+
+function handlePost() {
+    $.ajax({
+        url: '/equation',
+        method: 'POST',
+        data: postPackage,
+    }).then((response) => {
+        console.log(response)
+        postPackage = {};
+        console.log(postPackage);
+        handleGet();
+    })
+}
+
+
+function handleGet(){
+    $.ajax({
+        url: '/answerArray',
+        method: 'GET',
+    }).then((response) => {
+        console.log(response)
+        $('#history-list').empty();
+        for (let equation of response)
+        $('#history-list').append(`<li> ${equation.numberOne} ${equation.operator} ${equation.numberTwo} = ${equation.answer}
+        `)
+    })
 }
 
